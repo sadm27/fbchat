0185639d82a028976695f26fbecaa647034265f0
Sabbatini <sabbatinifrancois@gmail.com>
2019-05-09 16:35:32 -0400

    Create seeUnreadMessages.py
    Made and example for my fetch method

---
+++ examples/seeUnreadMessages.py
@@ -0,0 +1,19 @@
+from fbchat import Client
+from fbchat.models import *
+
+#example for the fetching all the unread messages from a Group or User conversation
+def getInfo(client :Client):
+
+    #Retrieves the all the unread message Group and User conversation threads
+    allTheUnreads = client.fetchUnread()
+
+    #Goes through each one of the threads that have unread messages in it
+    for allTheUnread in allTheUnreads:
+
+        #Prints out all the Unread messages
+        client.fetchUnreadFromThreadMessages(allTheUnread)
+
+if __name__ == "__main__":
+
+    client = Client("<email>", "<password>")
+    getInfo(client)
