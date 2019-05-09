b41149a4be0e987b43e11ca259b868601960fc18
Joel <sadm161@live.com>
2019-05-09 16:31:30 -0400

    Created tests
    Created a working set of test for video url issue and refactoring issue

--- tests/group-tests/test-fetch.py
+++ tests/group-tests/test-fetch.py
@@ -12,29 +12,43 @@ from fbchat.models import *
 
 client = Client('sadm161@live.com', 'Bravo127$')
 
-users = client.searchForUsers('Testo Accounto')
+#users = client.searchForUsers('Testo Accounto')
 
 
-def test_fetch_image_url_png(client):
-    client.sendLocalFiles([path.join(path.dirname(__file__), "resources", "image.png")])
-    message, = client.fetchThreadMessages(limit=1)
 
-    assert client.fetchImageUrl(message.attachments[0].uid)  == 9
 
-def test_fetch_image_url_gif(client):
-    client.sendLocalFiles([path.join(path.dirname(__file__), "resources", "image.gif")])
-    message, = client.fetchThreadMessages(limit=1)
+def test_fetch_image_url_png():
+    #client = Client('sadm161@live.com', 'Bravo127$')
+    users = client.searchForUsers('Testo Accounto')
+    user = users[0]
 
-    assert client.fetchImageUrl(message.attachments[0].uid)
+    messages = client.fetchThreadMessages(thread_id=user.uid, limit=4)
+    messages.reverse()
+    attach = messages[3].attachments[0]
 
-def test_fetch_video_url_mp4(client):
-    client.sendLocalFiles([path.join(path.dirname(__file__), "resources", "video.mp4")])
-    message, = client.fetchThreadMessages(limit=1)
+    assert ".png" in client.fetchImageUrl(attach.uid) 
 
-    assert client.fetchVideoUrl(message.attachments[0].uid)
 
-def test_fetch_video_url_webm(client):
-    client.sendLocalFiles([path.join(path.dirname(__file__), "resources", "video.webm")])
-    message, = client.fetchThreadMessages(limit=1)
+def test_fetch_image_url_jpg():
+    #client = Client('sadm161@live.com', 'Bravo127$')
+    users = client.searchForUsers('Testo Accounto')
+    user = users[0]
 
-    assert client.fetchVideoUrl(message.attachments[0].uid)
+    messages = client.fetchThreadMessages(thread_id=user.uid, limit=4)
+    messages.reverse()
+    attach = messages[2].attachments[0]
+
+    assert ".jpg" in client.fetchImageUrl(attach.uid)
+
+
+
+def test_fetch_video_url_mp4():
+    #client = Client('sadm161@live.com', 'Bravo127$')
+    users = client.searchForUsers('Testo Accounto')
+    user = users[0]
+
+    messages = client.fetchThreadMessages(thread_id=user.uid, limit=4)
+    messages.reverse()
+    attach = messages[1].attachments[0]
+
+    assert ".mp4" in client.fetchVideoUrl(attach.uid)
