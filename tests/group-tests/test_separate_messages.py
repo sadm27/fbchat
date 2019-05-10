75eef8c7b20616b6fd8cc73caa2ce61ec129f00e
Alex Pacheco <alexpacheco@charter.net>
2019-05-10 11:59:28 -0400

    automated tests for separating user messages

---
+++ tests/group-tests/test_separate_messages.py
@@ -0,0 +1,13 @@
+from fbchat import Client
+from fbchat import separate_messages
+
+testClient = Client('alexpacheco@charter.net', 'Q5pKJ9buWQ6v')
+
+def test_separateMessages():
+    sent_messages = separate_messages.get_sent_messages(testClient)
+    received_messages = separate_messages.get_received_messages(testClient)
+    for message_s in sent_messages:
+        for message_r in received_messages:
+            assert message_s.author != message_r.author
+
+
