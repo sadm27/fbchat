cdbff20aab4e70502534317b9bea7bde22c1eaa7
Alex Pacheco <alexpacheco@charter.net>
2019-05-10 12:00:49 -0400

    renamed file and added private functions to execute duplicate code in getting sent/received messages

---
+++ fbchat/separate_messages.py
@@ -0,0 +1,65 @@
+from fbchat import Client
+from fbchat.models import *
+
+
+def get_sent_messages(client:Client):
+    return __get_messages(client, True)
+
+
+def get_received_messages(client:Client):
+    return __get_messages(client, False)
+
+
+def print_single_thread(client:Client, thread_id):
+    messages = client.fetchThreadMessages(thread_id, limit=30)
+    thread_info = client.fetchThreadInfo(thread_id)
+    interlocutor = thread_info[thread_id]
+    __print_thread_messages(client, messages, interlocutor)
+
+
+def print_all_threads(client:Client):
+    threads = client.fetchThreadList()
+    for thread in threads:
+        messages = client.fetchThreadMessages(thread.uid, limit=30)
+        if thread.type == ThreadType.USER:
+            thread_info = client.fetchThreadInfo(thread.uid)
+            interlocutor = thread_info[thread.uid]
+            __print_thread_messages(client, messages, interlocutor)
+        else:
+            __print_thread_messages(client, messages, None)
+
+
+def __get_messages(client:Client, sent:bool):
+    threads = client.fetchThreadList()
+    sent_messages = []
+    received_messages = []
+    for thread in threads:
+        messages = client.fetchThreadMessages(thread.uid, limit=30)
+        name = client.fetchUserInfo(client.uid)[client.uid].name
+        user = client.searchForUsers(name)[0]
+        messages.reverse()
+        for message in messages:
+            if user.uid == message.author:
+                sent_messages.append(message)
+            elif user.uid != message.author:
+                received_messages.append(message)
+    if sent:
+        return sent_messages
+    else:
+        return received_messages
+
+
+def __print_thread_messages(client:Client, messages, interlocutor):
+    name = client.fetchUserInfo(client.uid)[client.uid].name
+    user = client.searchForUsers(name)[0]
+    print("\n--------------------------------------------------\n")
+    messages.reverse()
+    for message in messages:
+        if user.uid == message.author:
+            print(user.name, ": ", message.text)
+        elif interlocutor is not None and interlocutor.uid == message.author:
+            print(interlocutor.name, ":", message.text)
+        else:
+            print("Group Member :", message.text)
+    print("\n--------------------------------------------------\n")
+
--- fbchat/testproj.py
+++ fbchat/testproj.py
@@ -1,9 +1,9 @@
 from fbchat import Client
-from fbchat import user_messages
+from fbchat import separate_messages
 from fbchat.models import *
 
 
-client = Client('alexpacheco@charter.net', 'Jfp5Js9BDjs7')
+client = Client('alexpacheco@charter.net', 'Q5pKJ9buWQ6v')
 
 users = client.searchForUsers('Alex Pacheco')
 user = users[0]
@@ -13,6 +13,6 @@ print("User's name: {}".format(user.name))
 print("User's profile picture url: {}".format(user.photo))
 print("User's main url: {}".format(user.url))
 
-#user_messages.print_all_threads(client)
+separate_messages.print_all_threads(client)
 #thread1 = client.fetchThreadList()[0]
-#user_messages.print_single_thread(client, thread1.uid)
+#separate_messages.print_single_thread(client, thread1.uid)
