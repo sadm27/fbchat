979a8ad9cd9192b440e3f77fdfc14d312405d3c7
Alex Pacheco <alexpacheco@charter.net>
2019-05-11 14:05:15 -0400

    Update test_separate_messages.py

--- tests/group-tests/test_separate_messages.py
+++ tests/group-tests/test_separate_messages.py
@@ -1,15 +1,18 @@
 from fbchat import Client
 from fbchat import separate_messages
 
-testClient = Client('alexpacheco@charter.net', 'Q5pKJ9buWQ6v')
+test_client = Client('alexpacheco@charter.net', 'Q5pKJ9buWQ6v')
 
 
 def test_separate_messages():
-    sent_messages = separate_messages.get_sent_messages(testClient)
-    received_messages = separate_messages.get_received_messages(testClient)
+    sent_messages = separate_messages.get_sent_messages(test_client)
+    received_messages = separate_messages.get_received_messages(test_client)
     for message_s in sent_messages:
+        assert message_s.author == test_client.uid      # sent messages were written by user
         for message_r in received_messages:
-            assert message_s.author != message_r.author
+            assert message_s.author != message_r.author  # sent and received messages do not have same author
+    for message_r in received_messages:                 # separate for loop so that it does not get checked extra times
+        assert message_r.author != test_client.uid      # received messages were not written by user
 
 
-testClient.logout()
+test_client.logout()
