a08f51194f300d42ccc06c1f608dc781a865f8d9
Alex Pacheco <alexpacheco@charter.net>
2019-05-11 20:24:36 -0400

    Update test_separate_messages.py

--- tests/group-tests/test_separate_messages.py
+++ tests/group-tests/test_separate_messages.py
@@ -8,11 +8,11 @@ def test_separate_messages():
     sent_messages = separate_messages.get_sent_messages(test_client)
     received_messages = separate_messages.get_received_messages(test_client)
     for message_s in sent_messages:
-        assert message_s.author == test_client.uid      # sent messages were written by user
+        assert message_s.author == test_client.uid  # sent messages were written by user
         for message_r in received_messages:
-            assert message_s.author != message_r.author  # sent and received messages do not have same author
-    for message_r in received_messages:                 # separate for loop so that it does not get checked extra times
-        assert message_r.author != test_client.uid      # received messages were not written by user
+            assert message_s != message_r           # sent and received messages are not the same
+    for message_r in received_messages:             # separate for loop so that it does not get checked extra times
+        assert message_r.author != test_client.uid  # received messages were not written by user
 
 
 test_client.logout()
