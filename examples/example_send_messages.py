f188ab608ed9c55a30ac4e68c4d7714dc85b6717
Alex Pacheco <alexpacheco@charter.net>
2019-05-13 18:40:01 -0400

    Update example_send_messages.py

--- examples/example_send_messages.py
+++ examples/example_send_messages.py
@@ -1,11 +1,15 @@
 from fbchat import Client
 from fbchat.models import *
-
+import time
 
 client = Client('alexpacheco@charter.net', 'Q5pKJ9buWQ6v')
 
 client.wave(thread_id='1275720524', thread_type=ThreadType.USER)
 client.send(client, Message("This is a test message for my COMP 490 project."), '1275720524')
+client.send(client, Message("This message will be removed."), '1275720524')
+end_time = time.time() + 10
+while time.time() <= end_time:
+    x = 1   # wait
 message = client.fetchThreadMessages('1275720524', limit=1)
 client.unsend(message[0].uid)
 
