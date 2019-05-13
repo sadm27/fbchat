52108e73bed5652ac2a11b46407d8343a76e0c7c
Alex Pacheco <alexpacheco@charter.net>
2019-05-13 17:43:35 -0400

    updated send and separate message examples

--- examples/example_send_messages.py
+++ examples/example_send_messages.py
@@ -1,10 +1,10 @@
 from fbchat import Client
-from fbchat import separate_messages
 from fbchat.models import *
 
 
 client = Client('alexpacheco@charter.net', 'Q5pKJ9buWQ6v')
 
+client.wave(thread_id='1275720524', thread_type=ThreadType.USER)
 client.send(client, Message("This is a test message for my COMP 490 project."), '1275720524')
 message = client.fetchThreadMessages('1275720524', limit=1)
 client.unsend(message[0].uid)
