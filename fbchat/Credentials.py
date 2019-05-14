d808144b2dbd5c3905577ef28f0fec6ca5d0aad4
Alex Pacheco <alexpacheco@charter.net>
2019-05-13 20:24:55 -0400

    comment in credentials

--- fbchat/Credentials.py
+++ fbchat/Credentials.py
@@ -1,2 +1,5 @@
 username = 'sadm161@live.com'
 password = 'Bravo127$'
+
+''' video and picture fetching tests rely on username: 'sadm161@live.com' and password = 'Bravo127$'
+ due to message ordering in a specific thread connected to that account '''
